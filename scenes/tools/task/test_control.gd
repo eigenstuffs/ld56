a148c55ef68eebef6bce16ee8c51c6e2a5d692c3
DubiousDuck <118256740+DubiousDuck@users.noreply.github.com>
2024-10-05 00:13:20 -0700

    restructure + resource example

---
+++ scenes/tools/task/test_control.gd
@@ -0,0 +1,8 @@
+extends Control
+
+var task_list : TaskList = preload("res://scenes/station/task_list.tres")
+
+func _ready():
+	for index in range(get_child_count()):
+		get_child(index).value = task_list.task_array[index].total_time
+		print(task_list.task_array[index].total_time)
