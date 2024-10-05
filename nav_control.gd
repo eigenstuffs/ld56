e108772d4ce28a6e4de41a9a19dd039b890e340a
DubiousDuck <118256740+DubiousDuck@users.noreply.github.com>
2024-10-04 20:32:55 -0700

    pathfinding integration 1

--- nav_control.gd
+++ nav_control.gd
@@ -5,13 +5,13 @@ class_name Nav_Control
 @onready var goblin_folder = $"../GoblinFolder"
 @onready var goal_folder = $"../GoalFolder"
 
-var goblin_array : Array[Goblin] = []
+var goblin_array : Array[GoblinBase] = []
 var goal_array : Array[Goal] = []
 var agent_found : bool = false
 var goal_found : bool = false
 
 func _ready():
-	for goblin : Goblin in goblin_folder.get_children():
+	for goblin : GoblinBase in goblin_folder.get_children():
 		goblin_array.append(goblin)
 		goblin.connect("listening_for_target", _on_goblin_listening_for_target)
 	for goal : Goal in goal_folder.get_children():
@@ -21,19 +21,20 @@ func _ready():
 func _process(delta):
 	#logic: if both and agent and a goal are clicked, let agent go to the goal
 	if agent_found and goal_found:
-		var target_goblin : Goblin
+		var target_goblins : Array[GoblinBase]
 		var target_goal : Goal
-		for goblin : Goblin in goblin_array:
+		for goblin : GoblinBase in goblin_array:
 			if goblin.clicked:
-				target_goblin = goblin
+				target_goblins.append(goblin)
 				goblin.clicked = false
-				print("goblin is " + target_goblin.name)
+				print(goblin.name + " is gonna move") #TODO remove this
 		for goal : Goal in goal_array:
 			if goal.clicked:
 				target_goal = goal
 				goal.clicked = false
-				print("goal is " + target_goal.name)
-		target_goblin.set_movement_target(target_goal.global_position)
+				print("goal is " + target_goal.name) #TODO remove this
+		for goblin : GoblinBase in target_goblins:
+			goblin.set_movement_target(target_goal.global_position)
 		goal_found = false
 		agent_found = false
 
--- scenes/goblin/goblin.gd
+++ scenes/goblin/goblin.gd
@@ -2,17 +2,71 @@ extends CharacterBody2D
 
 class_name GoblinBase
 
-@export_enum(
-	"Idle",
-	"Navigate",
-	"Working",
-	"AwaitingInput",
-	"Explode"
-) var state = "Idle"
+enum STATE {
+	IDLE,
+	NAVIGATE,
+	WORKING,
+	AWAITING_INPUT,
+	EXPLODE
+}
+
+signal listening_for_target
+
+@onready var nav_agent = $NavigationAgent2D
+
+var movement_speed : float = 300
+var clicked : bool = false
+var state := STATE.IDLE
+
+func _ready():
+	
+	actor_setup.call_deferred()
+	
+func actor_setup():
+	# Wait for the first physics frame so the NavigationServer can sync.
+	await get_tree().physics_frame
 
 func _process(delta):
-	find_child(state).run()
+	#find_child(state).run()
+	
+	if clicked:
+		$Highlight.show()
+	else: $Highlight.hide()
 
 func change_state(to : String):
-	state = to
-	find_child(state).init()
+	var target_state
+	match to:
+		"Idle":
+			target_state = STATE.IDLE
+		"Navigate":
+			target_state = STATE.NAVIGATE
+		"Working":
+			target_state = STATE.WORKING
+		"AwaitInput":
+			target_state = STATE.AWAITING_INPUT
+		"Explode":
+			target_state = STATE.EXPLODE
+	state = target_state
+	find_child(to).init()
+
+func set_movement_target(movement_target : Vector2):
+	nav_agent.target_position = movement_target
+	state = 1 #navigate
+
+func _physics_process(delta):
+	if state == STATE.NAVIGATE:
+		if nav_agent.is_navigation_finished():
+			state = STATE.IDLE #idle
+			return
+		
+		var current_agent_position: Vector2 = global_position
+		var next_path_position: Vector2 = nav_agent.get_next_path_position()
+
+		velocity = current_agent_position.direction_to(next_path_position) * movement_speed
+		move_and_slide()
+
+func _on_clickable_region_gui_input(event):
+	if event.is_action_pressed("LMB"):
+		clicked = true
+		print("you clicked on this character!") #TODO remove this
+		emit_signal("listening_for_target")
