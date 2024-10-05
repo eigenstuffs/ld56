bf8ba361971f097a170efc42fb3bff1c6ecc5887
nylajafri <103155222+nylajafri@users.noreply.github.com>
2024-10-05 01:01:38 -0700

    in progress implementation of progress

--- scenes/menu/to_do_list_ui.gd
+++ scenes/menu/to_do_list_ui.gd
@@ -6,3 +6,6 @@ func _ready():
 	for index in range(get_child_count()):
 		get_child(index).value = task_list.task_array[index].total_time
 		print(task_list.task_array[index].total_time)
+
+
+#issue is logic- look at tomorrow when awake
