5971026b66afb50b1401e6ccb2b7cc666d1fd46a
DubiousDuck <118256740+DubiousDuck@users.noreply.github.com>
2024-10-05 16:36:44 -0700

    updated recipe and created test scene

--- scenes/menu/to_do_labels.gd
+++ scenes/menu/to_do_labels.gd
@@ -6,10 +6,11 @@ extends Label
 var task_list : TaskList = preload("res://scenes/tools/task/task_list.tres")
 
 func _ready():
-	var recipe_ingredients = task_list.task_array[0].ingredients
-
-	for index in range(min(4, recipe_ingredients.size())):
-		var ingredient_label = get_child(index)
-		if ingredient_label:
-			ingredient_label.text = recipe_ingredients[index]
-			print("Ingredient", index + 1, ":", recipe_ingredients[index])
+	pass
+	#var recipe_ingredients = task_list.task_array[0].ingredients
+#
+	#for index in range(min(4, recipe_ingredients.size())):
+		#var ingredient_label = get_child(index)
+		#if ingredient_label:
+			#ingredient_label.text = recipe_ingredients[index]
+			#print("Ingredient", index + 1, ":", recipe_ingredients[index])
