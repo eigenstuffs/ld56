6630a94663a8a3d50f088bad75cd76c30042a6ed
branden <brandenbohrnsen@gmail.com>
2024-10-05 10:24:11 +0930

    a

--- scenes/qte/mix.gd
+++ scenes/qte/mix.gd
@@ -3,11 +3,15 @@ class_name Mix extends Control
 @export var length : int = 4
 @export var combination : Array[String]
 
+const ARROW = preload("res://assets/arrow.tscn")
+
+@onready var arrows = $Arrows
+
 func _ready():
 	generate_combination(length)
 
-func generate_combination(length : int):
-	for i in length:
+func generate_combination(len : int):
+	for i in len:
 		var input : String
 		match randi_range(1, 4):
 			1:
@@ -19,4 +23,20 @@ func generate_combination(length : int):
 			4:
 				input = "right"
 		combination.append(input)
+		
+	create_icons()
 
+func create_icons():
+	for i in combination:
+		var a = ARROW.instantiate()
+		arrows.add_child(a)
+		var sprite = a.find_child("Sprite") as Sprite2D
+		match i:
+			"up":
+				sprite.frame_coords = Vector2(0, 1)
+			"down":
+				sprite.frame_coords = Vector2(3, 1)
+			"left":
+				sprite.frame_coords = Vector2(2, 1)
+			"right":
+				sprite.frame_coords = Vector2(1, 1)
