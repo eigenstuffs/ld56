63e9d73b64fc796cb854aeb61b8a72cf7c6341af
nylajafri <103155222+nylajafri@users.noreply.github.com>
2024-10-05 00:19:28 -0700

    edits to main menu and mock up of ui

--- scenes/menu/main_menu.gd
+++ scenes/menu/main_menu.gd
@@ -6,7 +6,7 @@ func _on_play_pressed() -> void:
 
 
 func _on_options_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/menu/OptionsMenu.tscn")
+	get_tree().change_scene_to_file("res://scenes/menu/Credits.tscn")
 
 
 func _on_quit_pressed() -> void:
