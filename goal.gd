fdecdc1258c95234703e8d6148301822106347a8
DubiousDuck <118256740+DubiousDuck@users.noreply.github.com>
2024-10-04 18:36:26 -0700

    pathfinding ver 2

--- goal.gd
+++ goal.gd
@@ -4,7 +4,10 @@ class_name Goal
 
 signal listening_for_agent
 
+var clicked : bool = false
+
 func _on_input_event(viewport, event, shape_idx):
 	if event.is_action_pressed("LMB"):
+		clicked = true
 		print("you clicked on this area!")
 		emit_signal("listening_for_agent")
--- nav_control.gd
+++ nav_control.gd
@@ -2,21 +2,42 @@ extends Node2D
 
 class_name Nav_Control
 
-@onready var test_goblin = $"../GoblinPrototype"
-@onready var test_goal = $"../Goal"
+@onready var goblin_folder = $"../GoblinFolder"
+@onready var goal_folder = $"../GoalFolder"
 
 var goblin_array : Array[Goblin] = []
+var goal_array : Array[Goal] = []
 var agent_found : bool = false
 var goal_found : bool = false
 
 func _ready():
-	goblin_array.push_back(test_goblin)
+	for goblin : Goblin in goblin_folder.get_children():
+		goblin_array.append(goblin)
+		goblin.connect("listening_for_target", _on_goblin_listening_for_target)
+	for goal : Goal in goal_folder.get_children():
+		goal_array.append(goal)
+		goal.connect("listening_for_agent", _on_goal_listening_for_agent)
 	
 func _process(delta):
+	#logic: if both and agent and a goal are clicked, let agent go to the goal
 	if agent_found and goal_found:
-		test_goblin.set_movement_target(test_goal.global_position)
+		var target_goblin : Goblin
+		var target_goal : Goal
+		for goblin : Goblin in goblin_array:
+			if goblin.clicked:
+				target_goblin = goblin
+				goblin.clicked = false
+				print("goblin is " + target_goblin.name)
+		for goal : Goal in goal_array:
+			if goal.clicked:
+				target_goal = goal
+				goal.clicked = false
+				print("goal is " + target_goal.name)
+		target_goblin.set_movement_target(target_goal.global_position)
+		goal_found = false
+		agent_found = false
 
-func _on_goblin_prototype_listening_for_target():
+func _on_goblin_listening_for_target():
 	agent_found = true
 
 func _on_goal_listening_for_agent():
