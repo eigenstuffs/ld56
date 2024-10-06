3411fac8617fb79cce9858c0419bb577cc217237
nylajafri <103155222+nylajafri@users.noreply.github.com>
2024-10-06 01:25:56 -0700

    add ingredient sprite

--- scenes/menu/to_do_list_ui.gd
+++ scenes/menu/to_do_list_ui.gd
@@ -12,9 +12,18 @@ var label_theme = preload("res://scenes/menu/testing_purpose/test_theme.tres")
 var recipes = []
 var current_recipe_index = 0
 
+var ingredient_sprites = {
+	"egg": "res://assets/ingredients/egg.png",
+	"flour": "res://assets/ingredients/flour.png",
+	"meat": "res://assets/ingredients/meat_frosted.png",
+	"secret": "res://assets/ingredients/secret_ingredient.png",
+	"spices": "res://assets/ingredients/spice.png",
+	"sugar": "res://assets/ingredients/sugar.png",
+	"veggie": "res://assets/ingredients/veggie.png"
+}
+
 func _ready():
 	recipes = [breakfast_recipe, soup_recipe, curry_recipe, dessert_recipe]
-	
 	show_recipe(current_recipe_index)
 
 func show_recipe(index):
@@ -29,19 +38,46 @@ func show_recipe(index):
 		recipe_container.add_child(dish_name_label)
 		
 		for i in range(recipe.required_ing.size()):
-			var ing_name: String = recipe.required_ing[i].ing_name
+			var ing_name = String(recipe.required_ing[i].ing_name) 
+			
+			if typeof(ing_name) != TYPE_STRING:
+				print("Unexpected type for ing_name:", typeof(ing_name))
+				continue
+
+			ing_name = format_ingredient_key(ing_name)
 			var req_amount: int = recipe.required_amount[i]
-			var current_amount: int = get_player_inventory_amount(ing_name)  # use actual game logic to fetch inventory info
+			var current_amount: int = get_player_inventory_amount(ing_name)
+
+			var hbox = HBoxContainer.new()
+
+			var texture = load_texture_for_ingredient(ing_name)
+			if texture:
+				var sprite = TextureRect.new()
+				sprite.texture = texture
+				hbox.add_child(sprite)
 
 			var ingredient_label = Label.new()
-			ingredient_label.text = ing_name + ": " + str(current_amount) + "/" + str(req_amount)
+			ingredient_label.text = ing_name.capitalize() + ": " + str(current_amount) + "/" + str(req_amount)
 			ingredient_label.theme = label_theme
+			hbox.add_child(ingredient_label)
 
-			print(str(recipe.enough(ing_name)))
-			
-			recipe_container.add_child(ingredient_label)
+			print("Sufficient ingredient?", recipe.enough(ing_name))
+
+			recipe_container.add_child(hbox)
 	else:
 		print("Recipe index is out of bounds")
+
+func load_texture_for_ingredient(ingredient_name: String) -> Texture:
+	if ingredient_name in ingredient_sprites:
+		var texture = load(ingredient_sprites[ingredient_name])
+		if texture:
+			print("Loaded texture for ingredient:", ingredient_name)
+		else:
+			print("Failed to load texture for ingredient:", ingredient_name)
+		return texture
+	else:
+		print("Ingredient name not in sprite map:", ingredient_name)
+	return null
 
 func clear_container(container: VBoxContainer):
 	for child in container.get_children():
@@ -50,7 +86,7 @@ func clear_container(container: VBoxContainer):
 
 func complete_recipe():
 	if current_recipe_index < recipes.size():
-		# add logic to complete recipe
+		
 		current_recipe_index += 1
 		if current_recipe_index < recipes.size():
 			show_recipe(current_recipe_index)
@@ -58,8 +94,11 @@ func complete_recipe():
 			print("All recipes completed!")
 
 func get_player_inventory_amount(ing_name: String) -> int:
-	# add inventory
+	# Placeholder for inventory
 	return 0
+
+func format_ingredient_key(ingredient_name: String) -> String:
+	return ingredient_name.strip_edges().to_lower()
 
 func _input(event):
 	if event.is_action_pressed("ui_accept"):
