de346419cfb942b2cf43d1710552ff27dc1e1693
branden <brandenbohrnsen@gmail.com>
2024-10-06 23:21:34 +1030

    goblin count for jobs

--- scenes/goblin/working.gd
+++ scenes/goblin/working.gd
@@ -2,18 +2,20 @@ extends GoblinState
 
 const JOB_PROGRESS : PackedScene = preload("res://scenes/goblin/job_progress.tscn")
 
-func job(area : Area2D):
-	if area.qte != null:
-		var a = area.qte.instantiate() as Mix
-		get_tree().current_scene.add_child(a)
-		a.global_position = get_parent().global_position - Vector2(0, 16)
-		await a.done
-		if a.result == "lose":
-			change_state("Eaten")
-	var b = JOB_PROGRESS.instantiate() as TextureProgressBar
-	add_child(b)
-	b.global_position = get_parent().global_position - Vector2(8, 24)
-	b.start(area.time)
-	await get_tree().create_timer(area.time).timeout
-	print("Done job")
-	change_state("JobDone")
+func job(area : Job):
+	area.goblins_engagaed += 1
+	if area.goblins_engagaed >= area.goblins_needed:
+		if area.qte != null:
+			var a = area.qte.instantiate() as Mix
+			get_tree().current_scene.add_child(a)
+			a.global_position = get_parent().global_position - Vector2(0, 16)
+			await a.done
+			if a.result == "lose":
+				change_state("Eaten")
+		var b = JOB_PROGRESS.instantiate() as TextureProgressBar
+		add_child(b)
+		b.global_position = get_parent().global_position - Vector2(8, 24)
+		b.start(area.time)
+		await get_tree().create_timer(area.time).timeout
+		print("Done job")
+		change_state("JobDone")
--- scenes/utilities/job.gd
+++ scenes/utilities/job.gd
@@ -3,7 +3,7 @@ class_name Job extends Area2D
 @export var time : int
 @export var qte : PackedScene
 @export var items_needed : Array
-@export var goblins_needed : int
+@export var goblins_needed : int = 1
 @export var goblins_engagaed : int = 0
 
 signal job_done
