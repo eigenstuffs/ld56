55233f4026ef60c4f666fe49a7d760870c699086
DubiousDuck <118256740+DubiousDuck@users.noreply.github.com>
2024-10-06 17:10:02 -0700

    updated getting and prepping

--- scenes/utilities/getting.gd
+++ scenes/utilities/getting.gd
@@ -2,6 +2,15 @@ extends Job
 
 class_name GettingJob
 
+@export var ingredient_box : Array[IngredientInfo]
+
+func init():
+	get_next_reward()
+
+func get_next_reward():
+	if ingredient_box.size() != 0:
+		item_reward = ingredient_box.pop_front()
+	
 func give_reward():
 	item_reward.state = 0
 	return item_reward
