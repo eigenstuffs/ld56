61d993945585940d8596253f52661cf6c269624a
cm600286 <91765679+cm600286@users.noreply.github.com>
2024-10-07 15:24:42 -0700

    fixed glitch

--- scenes/utilities/jobs/getting.gd
+++ scenes/utilities/jobs/getting.gd
@@ -6,11 +6,13 @@ class_name GettingJob
 @export var ingredient_box : Array[IngredientInfo]
 
 func pre_job(gob : GoblinBase):
+	item_reward = null
 	var task : ChoosingIngredient = choosing_task.instantiate()
 	get_tree().current_scene.add_child(task)
 	task.global_position = gob.global_position
 	await task.done
 	item_reward = task.selected
+	gob.hold_item(give_reward())
 	finish_pre_job()
 	task.queue_free()
 	
