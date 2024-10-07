c0b9c70ffc7659c0550fe76bef17b7cc2c014f6a
DubiousDuck <118256740+DubiousDuck@users.noreply.github.com>
2024-10-06 21:34:00 -0700

    updated score screen

--- scenes/utilities/score_screen.gd
+++ scenes/utilities/score_screen.gd
@@ -3,18 +3,31 @@ extends Control
 class_name ScoreScreen
 
 signal confirmed
+signal replay
 
 @onready var star_texture : Texture2D = preload("res://assets/deviled_egg.png") #TODO Replace Placeholder
+@onready var one_star : Texture2D = preload("res://assets/score_screen/1star.png")
+@onready var two_star : Texture2D = preload("res://assets/score_screen/2star.png")
+@onready var three_star : Texture2D = preload("res://assets/score_screen/3star.png")
+
 @onready var hbox : HBoxContainer = $CanvasLayer/TextureRect/HBoxContainer
 @onready var confirm_button = $CanvasLayer/Confirm
 @export var n_star : int
+@onready var textures : Array = [one_star, two_star, three_star]
 
 func init(number):
 	n_star = number
 	for n in range(n_star):
-		var a = TextureRect.new()
-		a.texture = star_texture
-		hbox.add_child(a)
+		$CanvasLayer/TextureRect.texture = textures[n]
+		await get_tree().create_timer(0.8).timeout
 
 func _on_confirm_pressed():
 	confirmed.emit()
+	print("confirmed")
+
+func _on_replay_pressed():
+	replay.emit()
+	print("replay")
+	
+func _ready():
+	init(3)
